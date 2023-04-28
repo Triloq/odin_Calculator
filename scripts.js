1f67c6db05ef7afce69df276d9d2822192f60b9a
Triloq <25014506+Triloq@users.noreply.github.com>
2023-04-28 16:09:17 -0500

    Limited number of sig. digits. SciNotation added.

--- scripts.js
+++ scripts.js
@@ -11,6 +11,9 @@ let operatorChoice = '';
 let tempValue = 0;
 let afterEquals = false;
 
+// this is the max number of significant digits to be shown after solution
+let screenLength = 10;
+
 // Set up digits
 const digits = document.querySelectorAll('.digit');
 digits.forEach(btn => {
@@ -41,20 +44,21 @@ operators.forEach(btn => {
         btn.classList.remove('operator');
         // Special case for equals
         if (btn.className === 'equals'){
-            if (afterEquals === false){
-                switch(true){
-                    case (tempInput === '' && heldValue != 0): finalValue = heldValue; break;
-                    case (tempInput != '' && heldValue === 0): finalValue = tempInput; break;
-                    default: finalValue = equals(heldValue,operatorChoice,tempInput); break;
-                }
-                screen.textContent = finalValue;
-                heldValue = finalValue;
-                fullInput = heldValue;
-                afterEquals = true;
+            // switch to tell if equals has been hit without new input or hit twice (on accident)
+            switch(true){
+                case (tempInput === '' && heldValue != 0): finalValue = heldValue; break;
+                case (tempInput != '' && heldValue === 0): finalValue = tempInput; break;
+                default: finalValue = equals(heldValue,operatorChoice,tempInput); break;
             }
+            // finalValue = manyDigits(finalValue);
+            screen.textContent = finalValue;
+            heldValue = finalValue;
+            fullInput = heldValue;
+            afterEquals = true;
+            
         }
-        else if (tempInput!= ''){   
-            console.log(`Full before: ${fullInput}`)
+        else if (tempInput!= ''){
+            // Will happen as long as there has been new digits pressed since last operator
             fullInput += btn.textContent; 
             if (heldValue != 0){               
                 tempValue = equals( heldValue, operatorChoice, tempInput);
@@ -63,9 +67,11 @@ operators.forEach(btn => {
             else {
                 heldValue = tempInput;         
             }         
-            console.log(`Full after: ${fullInput}`)
         }
-        
+        else {
+            // specific case of hitting another operator after an equals to continue equation
+            fullInput += btn.textContent;
+        }   
         // Adjust global variables
         operatorChoice = btn.className;
         tempInput = '';
@@ -88,6 +94,44 @@ const equals = ((a, op, b) => {
         case 'subtract': sol = subtract(a, b); break;
         default: break;
     }
+    // Change the number of sig digits and convert to scientific notation when greater than screenLength
+    sol = Math.round((sol + Number.EPSILON) * 1e10) / 1e10;
+    sol = parseFloat(Math.round((sol + Number.EPSILON) * 1e10) / 1e10);
+    sol = countSignificantIntegers(sol);
+    
     return sol;
 });
 
+const countSignificantIntegers = ((num, count = 0) => {
+
+    count = (num + '').replace(".", "").length;
+    if ( count > screenLength ){
+        return num.toExponential(5);
+    }
+    else {
+        return num;
+    }
+});
+
+
+// found at https://stackoverflow.com/questions/22884720/what-is-the-fastest-way-to-count-the-number-of-significant-digits-of-a-number
+// critical flaw that 0's before a decimal aren't considered significant for some reason
+// didn't use but keeping here for potential future
+var log10 = Math.log(10);
+function getSignificantDigitCount(n) {
+    n = Math.abs(String(n).replace(".", "")); //remove decimal and make positive
+    if (n == 0) return 0;
+    while (n != 0 && n % 10 == 0) n /= 10; //kill the 0s at the end of n
+
+    return Math.floor(Math.log(n) / log10) + 1; //get number of digits
+}
+
+const clearAll = (() => {
+    tempInput = '';
+    fullInput = '';
+    finalValue = 0;
+    heldValue = 0;
+    operatorChoice = '';
+    tempValue = 0;
+    afterEquals = false;
+})
